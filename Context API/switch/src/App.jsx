ece2de863bf6d4aba1d39e465d52ef91667176d0
xing24xing <khushipandey260304@gmail.com>
2024-08-28 22:26:58 +0530

    Added Toggle Files

--- Context API/switch/src/App.jsx
+++ Context API/switch/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider} from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <>
-      <h1 className='p-4 bg-pink-500 text-3xl'>Switch</h1>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -20,7 +37,7 @@ function App() {
               </div>
           </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
